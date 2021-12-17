dab551e8057098f2aaa70532ed81b8d94b651267
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2021-12-17 21:56:57 +0100

    Day17

--- AdventOfCode2021/TrajectoryAnalyzer.cs
+++ AdventOfCode2021/TrajectoryAnalyzer.cs
@@ -9,37 +9,46 @@ namespace AdventOfCode2021
 {
     public class TrajectoryAnalyzer
     {
+        public static IEnumerable<(int, int)> ComputeVelocities(((int, int), (int, int)) coordinates)
+        {
+            (int, int) cX = coordinates.Item1;
+            (int, int) cY = coordinates.Item2;
+            int x = 1000;
+
+            while (x > -1000)
+            {
+                int y = 1000;
+
+                --x;
+
+                while (y > -1000)
+                {
+                    --y;
+
+                    if (IterateShot(cX, cY, x, y) > Int32.MinValue)
+                        yield return (x, y);
+                }
+            }
+        }
+
         public static int Compute(((int, int), (int, int)) coordinates)
         {
             (int max, int x, int y) max = (int.MinValue, 0, 0);
             (int, int) cX = coordinates.Item1;
             (int, int) cY = coordinates.Item2;
-            int wayX = (cX.Item1 <= 0 ? -1 : 1);
-            int wayY = (cX.Item2 <= 0 ? -1 : 1);
-            
-            int x = 100000;
+            int x = cX.Item2;
 
-            while (x > -100000)
+            while (x > 0)
             {
-                int y = 100000;
+                int y = 1000;
                 
                 --x;
 
-                while (y > -100000)
+                while (y > -1000)
                 {
                     --y;
 
-                    int result = IterateShot(cX, cY, x, y)
-                        .Then(i =>
-                        {
-                            if (i.Any())
-                            {
-                                return i.OrderByDescending(i => i)
-                                    .FirstOrDefault();
-                            }
-
-                            return (max.max);
-                        });
+                    int result = IterateShot(cX, cY, x, y);
 
                     if (result > max.max)
                         max = (result, x, y);
@@ -49,33 +58,35 @@ namespace AdventOfCode2021
             return (max.max);
         }
 
-        public static IEnumerable<int> IterateShot((int, int) cX, (int, int) cY, int x, int y)
+        public static int IterateShot((int, int) cX, (int, int) cY, int x, int y)
         {
             int posX = 0;
             int posY = 0;
             int maxY = Int32.MinValue;
 
             while (posX is > -1000 and < 1000
-                   && posY is > -1000 and < 1000)
+                   && posY > -1000)
             {
-                if (maxY < y)
-                    maxY = y;
+                if (maxY < posY)
+                    maxY = posY;
 
                 if (posX >= cX.Item1 
                     && posX <= cX.Item2
                     && posY >= cY.Item1
                     && posY <= cY.Item2)
-                    yield return (maxY);
+                    return (maxY);
 
                 posX += x;
                 posY += y;
 
                 if (x > 0)
-                    ++x;
-                else if (x < 0)
                     --x;
+                else if (x < 0)
+                    ++x;
                 --y;
             }
+
+            return (Int32.MinValue);
         }
     }
 }
